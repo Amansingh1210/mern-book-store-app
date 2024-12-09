9ba8548243369a2dc308b2ca1e28665a23cefd2e
Aman Singh <singhaman38809@gmail.com>
2024-12-09 21:41:28 +0530

    Day 2 : Banner section is completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 
@@ -6,7 +7,7 @@ function App() {
     <>
       <Navbar />
       <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
-        
+        <Outlet />
       </main>
       <footer>footer</footer>
     </>
--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -1,10 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Home from "../pages/home/home";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />
+        element: <App />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/orders',
+                element: <div> Orders</div >
+            },
+            {
+                path: '/about',
+                element: <div> about</div >
+            },
+        ]
     },
 ]);
 
