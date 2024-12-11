a9200de8876b06e824453fed61ba87d39ca13080
Aman Singh <singhaman38809@gmail.com>
2024-12-11 20:48:41 +0530

    Day 4 : Recommended books section complete

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import TopSallers from './TopSallers'
+import Recommended from './Recommended'
 
 const Home = () => {
   return (
     <>
         <Banner />
         <TopSallers />
+        <Recommended />
     </>
   )
 }
